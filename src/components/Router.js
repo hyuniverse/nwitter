a7ea7f57ba0d7cac91103befea073a7501a0550e
hyuniverse <89325462+hyuniverse@users.noreply.github.com>
2022-07-23 15:00:15 +0900

    Update up to 4.0

---
+++ src/components/Router.js
@@ -0,0 +1,27 @@
+import { HashRouter as Router, Navigate, Route, Routes } from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "./Navigation";
+
+const AppRouter = ({ isLoggedIn }) => {
+    return (
+        <Router>
+            {isLoggedIn && <Navigation />}
+            <Routes>
+                {isLoggedIn ? (
+                    <>
+                        <Route path="/" element={ <Home /> } />
+                        <Route path="/profile" element={ <Profile /> } />
+                    </>
+                ):(
+                    <Route path="/" element={ <Auth /> } />
+                )}
+                <Route path="*" element={<Navigate replace to="/"/>} />
+            </Routes>
+        </Router>
+    )
+}
+
+export default AppRouter;
+
